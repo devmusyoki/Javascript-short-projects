5536484a0e85f27dae2e8d16bef174d4f7a72aff
smusyoki <markmusyoki6@gmail.com>
2021-07-26 12:31:09 +0300

    success/failure functions added

--- Login-form Validation/main.js
+++ Login-form Validation/main.js
@@ -1,9 +1,13 @@
 // DOM Elements
-const loginPassword = document.getElementById("loginPassword")
+const loginContainer = document.querySelector('.login-container');
+const loginForm = document.getElementById("loginForm");
+const loginPassword = document.getElementById("loginPassword");
 const showPasswordIcon = document.querySelector("i.fa-eye");
-
+const loginUsername = document.getElementById("loginUsername");
 showPasswordIcon.addEventListener('click', showPassword);
 
+
+//Show Hide Password
 function showPassword() {
    // console.log("loginPassword");
 
@@ -14,7 +18,57 @@ function showPassword() {
        loginPassword.type = 'password';
        showPasswordIcon.style.color = "#ddd"
    }
-
-
 }
 //console.log(showPasswordIcon)
+
+
+// Simple Validation
+loginForm.addEventListener('submit',submitValidation);
+
+function submitValidation(event) {
+    event.preventDefault();
+// Username Validation
+
+  if (loginUsername.value=== "" || loginUsername.value.length < 3){
+     // console.log("empty")
+     showError(loginUsername, "Username cannot be empty & must be > 3");
+  } else {
+    //  console.log("not-empty")
+   showSuccess(loginUsername);
+  } 
+
+//   Password Validation
+
+  if(loginPassword.value=== "" || loginPassword.value.length < 6){
+      showError(loginPassword, "password cannot be empty or less than 6");
+  }else {
+     // console.log("not-empty")
+     showSuccess(loginPassword, );
+}
+}
+
+// show error message
+function showError(input, message){
+    const formField = input.parentElement;
+  //  console.log(formField.className);
+
+formField.className = "form-field error";
+if ((formField.className="form-field error")) {
+    const alert_message = formField.querySelector('.alert-message');
+    alert_message.style.visibility = "visible";
+    alert_message.style.color= "red";
+    alert_message.innerText = message
+}
+}
+
+//show success message
+function showSuccess(input){
+    const formField = input.parentElement;
+   
+    formField.className = "form-field success";
+    if ((formField.className="form-field success")){
+        const alert_message = formField.querySelector('.alert-message');
+        alert_message.style.visibility = "hidden";
+        
+    }
+}
